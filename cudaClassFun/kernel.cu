1408dfb504a97dd9ae0013a1e84e64e7bcce8f31
Jakub Niedzwiedzki <jakub1niedzwiedzki@gmail.com>
2018-11-19 18:30:40 +0100

    still having fun

--- cudaClassFun/kernel.cu
+++ cudaClassFun/kernel.cu
@@ -2,6 +2,7 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 #include "thrust/device_vector.h"
+#include "curand.h"
 #include  <iostream>
 #include <chrono>
 #include <stdio.h>
@@ -34,8 +35,18 @@ class Dummy : public Managed
 {
 public:
 	int counter;
+	double magicNumber;
 	Dummy() : counter(0) { ; }
-	__device__ void incrementCounter() { ++counter; }
+	__device__ void incrementCounter()
+	{
+		++counter;
+		magicNumber = counter * 5 * pow((double)3, __double2int_rd(magicNumber) % 100) + counter * magicNumber + counter * 2 * pow((double)2, __double2int_rd(magicNumber) % 50) + counter * magicNumber;
+	}
+	void incrementHost()
+	{
+		++counter;
+		magicNumber = counter * 5 * pow(3, ((int)magicNumber % 100)) + counter * magicNumber + counter * 2 * pow(2, ((int)magicNumber % 50)) + counter * magicNumber;
+	}
 };
 
 __global__ void incrementDummy(Dummy *a)
@@ -45,7 +56,7 @@ __global__ void incrementDummy(Dummy *a)
 }
 int main()
 {
-    auto constexpr arraySize = 500000000;
+    auto constexpr arraySize = 10000000;
 	Dummy *arrPtr = new Dummy[arraySize];
 	auto start = std::chrono::high_resolution_clock::now();
 	incrementDummy <<< (arraySize + 255) / 256, 256 >>> (arrPtr);
@@ -58,7 +69,7 @@ int main()
 	auto start2 = std::chrono::high_resolution_clock::now();
 	for (auto i = 0; i < arraySize; ++i)
 	{
-		arrPtr[i].counter++;
+		arrPtr[i].incrementHost();
 	}
 	auto finish2 = std::chrono::high_resolution_clock::now();
 	std::chrono::duration<double> elapsed2 = finish2 - start2;
